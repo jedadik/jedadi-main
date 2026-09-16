06292e4bb432d14da3111c41e5e6e6a7c59abe54
jedadik <hoyosdiaz726@gmail.com>
2026-09-15 23:11:49 -0500

    Nuevo link

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,5 +1,5 @@
 export const DASHBOARD_URL = "https://dashboard.jedadi.com/";
-export const TECNOSOLAR_URL = "https://webdeployhub.github.io/tecnosolarDelcaribe/";
+export const TECNOSOLAR_URL = "https://tecnosolar.jedadi.com/";
 export const TIKTOK_URL = "https://www.tiktok.com/@jesus00458?_r=1&_t=ZS-99PgruYZEVJ";
 export const INSTAGRAM_URL = "https://www.instagram.com/jesusdiaz004?igsi=dXpxMDlndWpiYXB2&utm_source=qr";
 export const FACEBOOK_URL = "https://www.facebook.com/share/1ERkABqQmT/?mibextid=wwXIfr";
